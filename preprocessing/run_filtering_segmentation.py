ce73f77dce0307e13d59fb2fdf7187c70954ca1f
Alex M <alexm@alexm.com>
2023-05-04 10:58:20 -0700

    run_filtering_segmentation.py - cleaned up unused code and old comments

--- preprocessing/run_filtering_segmentation.py
+++ preprocessing/run_filtering_segmentation.py
@@ -44,20 +44,16 @@ if __name__ == '__main__':
 	import os
 	import os.path as path
 	from edf_preprocessing import PreProcessing
+	from edf_preprocessing import slice_edfs
 
 	# Inputs
 	N_ARRAY_JOBS = 100	# Number of parrallel jobs to run on cluster
 	#hospital = 'Burnaby'	# Either Burnaby or Abbotsford
 	hospital = 'Abbotsford'	# Either Burnaby or Abbotsford
-	bRunSingleFile = False	# Flag to test single PreProcessing class instance
 
 	data_root, out_root, cluster_job = get_data_folders()
 
 	# Burnaby
-	file_name = "81c0c60a-8fcc-4aae-beed-87931e582c45.edf"
-	#file_name = "81be60fc-ed17-4f91-a265-c8a9f1770517.edf"
-	#file_name = "fff0b7a0-85d6-4c7e-97be-8ae5b2d589c2.edf"
-	#file_name = "ffff1021-f5ba-49a9-a588-1c4778fb38d3.edf"
 	#source_scan_ids = ["fff0b7a0-85d6-4c7e-97be-8ae5b2d589c2", "ffff1021-f5ba-49a9-a588-1c4778fb38d3"]
 	#source_scan_ids = [
         #        '819ebadf-1bcb-4c35-8280-fb63d4747b35','ffedacda-ce90-452c-8007-f46ec1a04cd1',
@@ -65,45 +61,13 @@ if __name__ == '__main__':
 	#	'81aa06db-db61-45af-965d-71813cf34a81','ffef5962-ed51-45d6-b20a-a95dd1f6ddde',
 	#	'81be60fc-ed17-4f91-a265-c8a9f1770517','fff0b7a0-85d6-4c7e-97be-8ae5b2d589c2',
 	#	'81c0c60a-8fcc-4aae-beed-87931e582c45','ffff1021-f5ba-49a9-a588-1c4778fb38d3']
-	source_scan_ids = None
 
 	# Abbotsford
-	#file_name = "fffcedd0-503f-4400-8557-f74b58cff9d9.edf"	# HV
-	#file_name = "fffaab93-e908-4b93-a021-ab580e573585.edf"	# No HV
-	#file_name = "test_HV.edf"				# Several HV series
+	#source_scan_ids = ["1a02dfbb-2d24-411c-ab05-1a0a6fafd1e5"]	# Here annots not cropped properly in prev version
+	source_scan_ids = None
 
 	input_dir = data_root + "/" + hospital
 	output_dir = out_root + "/" + hospital
-	pathname = input_dir + '/' + file_name
-
-	# These will be used if explicitly specified in the PreProcessing class constructor
-	target_frequency=200
-	lfreq=0.5
-	hfreq=55
-
-	if bRunSingleFile:
-		# Initiate the preprocessing object, filter the data between 0.5 Hz and 55 Hz and resample to 200 Hz.
-		# p = PreProcessing(pathname, target_frequency=target_frequency, lfreq=lfreq, hfreq=hfreq)
-		p = PreProcessing(pathname)		# All parms will be read from the JSON conf file
-
-		# This calls internal functions to detect 'bad intervals' and define 5 'good' ones 60 seconds each
-		# p.extract_good(target_length=target_length, target_segments=target_segments) QQQ
-		p.extract_good()
-
-		# import sys
-		# sys.exit()	# QQQQQ
-
-		# Calling the function saves new EDF files to output_folder. In case there are more than 1, it adds suffix "_n" to the file name 
-		p.save_edf(folder=output_dir, filename=file_name)
-
-		# Extract and convert data to Numpy arrays
-		p.create_intervals_data()
-		df = p.intervals_df
-
-		print('\nDone processing a single file {} using the PreProcessing class.\n'.format(file_name))
-
-	# Test processing multiple records using slice_edfs() function
-	from edf_preprocessing import slice_edfs
 
 	# When running on the CC cluster, 1st command line argument is a 0-based
 	# array job index
